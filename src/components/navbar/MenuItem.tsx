3dec584304748ef71b32b81e5fd03689787d3e4d
Ruslan <ruslan.mirets@gmail.com>
2023-05-31 15:36:41 +0300

    Navbar UI

---
+++ src/components/navbar/MenuItem.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { FC } from "react";
+
+interface IMenuItem {
+	onClick: () => void;
+	label: string;
+}
+
+const MenuItem: FC<IMenuItem> = ({ onClick, label }) => {
+	return (
+		<div
+			className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+			onClick={onClick}
+		>
+			{label}
+		</div>
+	);
+};
+
+export default MenuItem;
