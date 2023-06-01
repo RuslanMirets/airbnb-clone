ca46ffd8aa15664aed2024f2f838b130b7e1068c
Ruslan <ruslan.mirets@gmail.com>
2023-06-01 17:09:25 +0300

    Categories UI

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const Logo = () => {
-	const {} = useRouter();
+	const router = useRouter();
 
 	return (
 		<Image
@@ -13,6 +13,7 @@ const Logo = () => {
 			width={100}
 			height={100}
 			alt="Logo"
+			onClick={() => router.push("/")}
 		/>
 	);
 };
